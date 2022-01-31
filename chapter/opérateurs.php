e60b1f1e9bd65ae7b3dabcc7dae20b07856ddb12
Amaranth01 <vanessa.nectoux@gmail.com>
2022-01-31 16:37:57 +0100

    Début de révision

---
+++ chapter/opérateurs.php
@@ -0,0 +1,15 @@
+<?php
+
+$nombre = 758;
+$chaine1 = "j'affiche mon nombre : " . $nombre;
+echo $chaine1 . "<br>";
+
+$nombre2 = 254;
+$total = $nombre + $nombre2 . "<br>";
+
+$total2 = $nombre / $nombre2 . "<br>";
+
+$total3 = $nombre % $nombre2 . "<br>";
+
+$total4 = $nombre ** $nombre2 . "<br>";
+
---
+++ chapter/variable .php
@@ -0,0 +1,11 @@
+<?php
+
+$nombre1 = 5;
+$chaine = "Coucou";
+
+echo $nombre1 +2 . "<br>";
+echo $chaine . "<br>";
+
+echo gettype($nombre1) . "<br>";
+
+var_dump($chaine);
